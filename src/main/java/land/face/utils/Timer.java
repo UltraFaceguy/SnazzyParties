373c1ca361a4e66a1ea0bd27b3c4f38cf0068a59
BuildTools <unconfigured@null.spigotmc.org>
2019-11-27 20:08:59 -0600

    Major Changes

--- src/main/java/land/face/utils/Timer.java
+++ src/main/java/land/face/utils/Timer.java
@@ -1,14 +1,14 @@
-package land.face.managers;
+package land.face.utils;
 
 import land.face.SnazzyPartiesPlugin;
 import land.face.data.Party;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class SnazzyPartiesTimer extends BukkitRunnable {
+public class Timer extends BukkitRunnable {
 
     Party party;
 
-    public SnazzyPartiesTimer(Party party) {
+    public Timer(Party party) {
         this.party = party;
         runTaskTimer(SnazzyPartiesPlugin.getInstance(), 0L, 2L);
     }
