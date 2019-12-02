d7e8c771addcb7ca5efd38a7e24543916dbc428d
UltraFaceguy <kattreinkyle@gmail.com>
2019-12-02 18:01:16 -0500

    Handling non-standard attack methods

--- src/main/java/land/face/listeners/DamageListener.java
+++ src/main/java/land/face/listeners/DamageListener.java
@@ -1,7 +1,11 @@
 package land.face.listeners;
 
 import land.face.SnazzyPartiesPlugin;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EvokerFangs;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -17,15 +21,29 @@ public class DamageListener implements Listener {
 
   @EventHandler(priority = EventPriority.LOWEST)
   public void onDamage(EntityDamageByEntityEvent e) {
-      if (e.getDamager() instanceof Player && e.getEntity() instanceof Player){
-          Player attacker = (Player) e.getDamager();
-          Player victim = (Player) e.getEntity();
-          if (plugin.getSnazzyPartiesManager().areInSameParty(attacker, victim) && plugin.getSnazzyPartiesManager().getParty(victim).getFriendlyFire()){
-              e.isCancelled();
-          }
-
-      }
-
+    if (!(e.getEntity() instanceof Player)) {
+      return;
+    }
+    LivingEntity attacker = getAttacker(e.getDamager());
+    if (!(attacker instanceof Player)) {
+      return;
+    }
+    if (plugin.getPartyManager().areInSameParty((Player) e.getEntity(), (Player) attacker) && plugin
+        .getPartyManager().getParty((Player) e.getEntity()).getFriendlyFire()) {
+      e.isCancelled();
+    }
   }
 
+  private static LivingEntity getAttacker(Entity entity) {
+    if (entity instanceof LivingEntity) {
+      return (LivingEntity) entity;
+    } else if (entity instanceof Projectile) {
+      if (((Projectile) entity).getShooter() instanceof LivingEntity) {
+        return (LivingEntity) ((Projectile) entity).getShooter();
+      }
+    } else if (entity instanceof EvokerFangs) {
+      return ((EvokerFangs) entity).getOwner();
+    }
+    return null;
+  }
 }
