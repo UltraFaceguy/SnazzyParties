6072293a16632f5807e450c917bd8a159ad45f5e
Lozke <joshuadavis1403@gmail.com>
2019-12-03 03:31:09 -0600

    Resolves issue with re-logging players not appearing to be in party

--- src/main/java/land/face/managers/PartyManager.java
+++ src/main/java/land/face/managers/PartyManager.java
@@ -177,7 +177,7 @@ public class PartyManager {
   public Party getParty(UUID uuid) {
     for (Party party : parties) {
       for (PartyMember member : party.getMembers()) {
-        if (member.getUUID() == uuid) {
+        if (member.getUUID().toString().equals(uuid.toString())) {
           return party;
         }
       }
