70f70f77f64d7305fa98dcf1f8366c46bb14af1b
UltraFaceguy <kattreinkyle@gmail.com>
2019-12-03 20:19:54 -0500

    Making offline timeouts and invite timeouts configurable

--- src/main/java/land/face/data/Invitation.java
+++ src/main/java/land/face/data/Invitation.java
@@ -2,23 +2,20 @@ package land.face.data;
 
 public class Invitation {
 
-    private Party party;
-    private long timestamp;
+  private Party party;
+  private long timestamp;
 
-    public Invitation(Party party){
-        this.party = party;
-        timestamp = System.currentTimeMillis();
-    }
+  public Invitation(Party party) {
+    this.party = party;
+    timestamp = System.currentTimeMillis();
+  }
 
-    public Party getParty() {
-        return party;
-    }
+  public Party getParty() {
+    return party;
+  }
 
-    public long getTimestamp() {
-        return timestamp;
-    }
+  public long getTimestamp() {
+    return timestamp;
+  }
 
-    public boolean isValid() {
-        return System.currentTimeMillis() - timestamp  < 600000;
-    }
 }
