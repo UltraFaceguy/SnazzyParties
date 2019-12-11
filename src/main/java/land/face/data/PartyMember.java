b93c60e40a0e7b34b014ed6cf8f43c96ab0ba3fb
Lozke <joshuadavis1403@gmail.com>
2019-12-10 19:55:25 -0600

    Added scoreboard toggle boolean for show/hide command

--- src/main/java/land/face/data/PartyMember.java
+++ src/main/java/land/face/data/PartyMember.java
@@ -8,10 +8,12 @@ public class PartyMember {
   private UUID uuid;
   private String username;
   private long quitTimestamp;
+  private Boolean scoreboardToggle;
 
   public PartyMember(Player player) {
     this.uuid = player.getUniqueId();
     this.username = player.getDisplayName();
+    this.scoreboardToggle = true;
   }
 
   public UUID getUUID() {
@@ -30,4 +32,11 @@ public class PartyMember {
     this.quitTimestamp = quitTimestamp;
   }
 
+  public Boolean getScoreboardToggle() {
+    return scoreboardToggle;
+  }
+
+  public void setScoreboardToggle(Boolean scoreboardToggle) {
+    this.scoreboardToggle = scoreboardToggle;
+  }
 }
