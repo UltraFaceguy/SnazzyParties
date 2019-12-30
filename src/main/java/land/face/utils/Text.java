c6c120db0dc367bdcfd7cdd98949953b207b4537
Lozke <joshuadavis1403@gmail.com>
2019-12-29 22:26:35 -0600

    Added config handler to replace config message variables

--- src/main/java/land/face/utils/Text.java
+++ src/main/java/land/face/utils/Text.java
@@ -1,9 +1,23 @@
 package land.face.utils;
 
+import land.face.SnazzyPartiesPlugin;
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
 public class Text {
     public static String colorize(String message){
         return message.replaceAll("&([0-9a-fk-or])", ChatColor.COLOR_CHAR + "$1");
+    }
+    public static String configHandler(Player player, String message) {
+        message = message.replace("{name}", player.getDisplayName());
+        message = message.replace("{prefix}", SnazzyPartiesPlugin.getInstance().getPartyManager().getPrefix());
+        message = PlaceholderAPI.setPlaceholders(player, message);
+        return colorize(message);
+    }
+    public static String configHandler(String username, String message) {
+        message = message.replace("{name}", username);
+        message = message.replace("{prefix}", SnazzyPartiesPlugin.getInstance().getPartyManager().getPrefix());
+        return colorize(message);
     }
 }
